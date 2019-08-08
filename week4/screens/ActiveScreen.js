36bb8712d6af04d309ca76386298cdfb69ecac40
Khanh Tran-Cong <congkhanh197@gmail.com>
2019-08-08 19:10:13 +0700

    [week4] Init project

---
+++ week4/screens/ActiveScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text } from "react-native";
+
+export default function ActiveScreen() {
+  /**
+   * Go ahead and delete ExpoConfigView and replace it with your content;
+   * we just wanted to give you a quick view of your config.
+   */
+  return <Text>test</Text>;
+}
+
+ActiveScreen.navigationOptions = {
+  title: "Active"
+};
